d3968bb95989b36507a468814acf0e3200e046a2
salmanebaba <g130172668@taalim.ma>
2025-02-01 04:29:13 +0100

    myTemplate

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var options = {
+        strings: ["Engineering Student", "Cloud - IoT Enthusiast", "Embedded Systems Developer"],
+        typeSpeed: 50,
+        backSpeed: 25,
+        loop: true
+    };
+    var typed = new Typed(".typing", options);
+});
